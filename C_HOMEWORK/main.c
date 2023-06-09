786d2aea0a82daa04a31ca2bbbd4a109825995cc
Fevzi Yüksel <fevziyuksel1996@gmail.com>
2023-06-09 23:56:19 +0300

    update

--- C_HOMEWORK/main.c
+++ C_HOMEWORK/main.c
@@ -2,23 +2,5 @@
 #include "homework.h"
 #include "list.h"
 
-int words(const char* str)
-{
-	int begin = 1, c = 0;
 
-	for (int i = 0; i != '\0'; i++)
-	{
-		
-
-	}
-
-
-	return 0;
-}
-
-
-//int main(int argc, char* argv[])
-//{
-//	return 0;
-//}
 
