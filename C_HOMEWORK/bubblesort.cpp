889754420424528b751b3ea93009c682a9cb392c
Fevzi Yüksel <fevziyuksel1996@gmail.com>
2023-06-22 22:33:30 +0300

    update

--- C_HOMEWORK/bubblesort.cpp
+++ C_HOMEWORK/bubblesort.cpp
@@ -4,6 +4,20 @@
 #include <random>
 
 namespace fy {
+
+	template<typename T>
+	using comparator_func = std::function<bool(const T&, const T&)>;
+
+	template<typename T>
+	struct comparator
+	{
+		bool operator() (const T& lval, const T& rval)
+		{
+			return lval > rval;
+		}
+	};	
+
+
 	template<typename T>
 	constexpr void swap(T& lvalue, T& rvalue) noexcept
 	{
@@ -13,7 +27,7 @@ namespace fy {
 	}
 
 	template<typename T>
-	void bubblesort(std::vector<T>& arr, const std::function<bool(const T&, const T&)>& compare) noexcept
+	void bubblesort(std::vector<T>& arr, const fy::comparator_func<T>& compare = [](T x, T y) -> bool {return x > y; }) noexcept
 	{
 		bool is_swapped = false;
 		for (size_t i = 0; i < arr.size() - 1; i++)
@@ -21,11 +35,8 @@ namespace fy {
 			is_swapped = false;
 			for (size_t j = 0; j < arr.size() - 1 - i; j++)
 			{
-				if (compare(arr[j], arr[j + 1]))
-				{
+				if (is_swapped = compare(arr[j], arr[j + 1]))
 					fy::swap<T>(arr[j], arr[j + 1]);
-					is_swapped = true;
-				}
 			}
 
 			if (!is_swapped)
@@ -38,14 +49,25 @@ namespace fy {
 	void print_arr(const std::vector<T>& vec) noexcept
 	{
 		for (const T& e : vec)
-			std::cout << e << '\n';
+			std::cout << e << " ";
+			
+		std::cout << std::endl;
+	}
+
+	
+	template<typename T, class Iterator>
+	void print_container(const Iterator& begin, const Iterator& end, size_t size)
+	{
+		int ctr = 0;
+		for (Iterator i = begin; ctr++ < size && begin != end; ++i)
+			std::cout << *i << " ";
 			
 		std::cout << std::endl;
 	}
 
 	//Copy Elision
 	template<typename T>
-	std::vector<T> random_vector(const T begin, const T end, size_t size)
+	std::vector<T> random_vector(const T& begin, const T& end, size_t size)
 	{
 		std::random_device rand;
 
@@ -62,18 +84,44 @@ namespace fy {
 		return vec;
 	}
 
+	//Don't work any container except std::vector
+	template<typename T, class Container>
+	Container random_container(const T& begin, const T& end, size_t size)
+	{
+		std::random_device rand;
+
+		std::mt19937_64 gen(rand());
+
+		std::uniform_int_distribution<T> dist(begin, end);
+
+		Container container;
+
+		container.reserve(size);
+
+		for (int i = 0; i < size; ++i)
+			container.emplace_back(std::move(dist(gen)));
+
+		return container;
+	}
+
 }
 
-int maincpp()
+int main()
 {
-	//Copy Elision
-	auto vec = fy::random_vector<int>(-50, 50, 1);
+	using uint32 = unsigned int;
+
+	auto vec = fy::random_vector<uint32>(-50, 50, 10);
+
+	fy::bubblesort<uint32>(vec);
+	fy::bubblesort<uint32>(vec, [](uint32 x, uint32 y) {return x > y; });
+	fy::bubblesort<uint32>(vec, fy::comparator<uint32>());
+
+	fy::comparator<uint32> c;
+
+	fy::bubblesort<uint32>(vec, c);
 
 
-	//auto comperator = [](int x, int y) {return x > y; };
-	////fy::bubblesort<int>(vec, comperator);
-	//
-	//fy::print_arr<int>(vec);
+	fy::print_container<uint32, std::vector<uint32>::const_iterator>(vec.begin(), vec.end(), vec.size());
 
 	return 0;
 }
--- C_HOMEWORK/main.c
+++ C_HOMEWORK/main.c
@@ -2,8 +2,8 @@
 #include "homework.h"
 
 
-int main(int argc, char* argv[])
-{
-	return 0;
-}
+//int main(int argc, char* argv[])
+//{
+//	return 0;
+//}
 
